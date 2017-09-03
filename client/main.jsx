f6f9953ac22d311ddac50004a86d773b313fbe8a
Ricky Lung <dard12@gmail.com>
2017-09-03 17:44:18 -0400

    first

---
+++ client/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+import App from '../imports/app.jsx';
+
+Meteor.startup(() => {
+  _ = lodash;
+  render(<App />, document.getElementById('render-target'));
+});
