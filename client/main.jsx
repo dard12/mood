bd05023cac9bd28feead13bbeba8ed9019f3a6c3
Ricky Lung <dard12@gmail.com>
2017-09-04 16:31:53 -0400

    add routing

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Journal from '/imports/journal.jsx';
+import Emotion from '/imports/emotion.jsx';
 
 Meteor.startup(() => {
-  render(<Journal />, document.getElementById('render-target'));
+  render(
+    <Router>
+      <Route path="/" component={Journal}>
+        <Route path="edit-entry" component={Emotion} />
+      </Route>
+    </Router>,
+    document.getElementById('root')
+  );
 });
